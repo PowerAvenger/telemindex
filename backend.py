deefa70aff2888fc48a8cc365dba1711e9b9d227
Jose <>
2024-10-29 19:05:53 +0100

    añadido porcentaje componentes y modficado layout

--- backend.py
+++ backend.py
@@ -114,8 +114,62 @@ def pt1():
         index='hora',
         aggfunc='mean'
     ).reset_index()
+
+    pt20=dffm.pivot_table(
+        values=['spot', 'ssaa', 'osom', 'Otros', 'ppcc_2.0', 'perd_2.0', 'pyc_2.0'],
+        index='año',
+        aggfunc='mean'
+    )
+    pt20['comp_perd']=pt20['spot']+pt20['ssaa']+pt20['osom']+pt20['Otros']+pt20['ppcc_2.0']
+    pt20['perdidas_2.0']=pt20['comp_perd']*(pt20['perd_2.0'])
+    pt20=pt20.drop(columns=['perd_2.0','comp_perd'])
+    pt20_trans=pt20.transpose().reset_index()
+    pt20_trans=pt20_trans.rename(columns={'index':'componente',2024:'valor'})
+    pt20_trans['componente'] = pt20_trans['componente'].replace({'Otros': 'otros'})
+    pt20_trans=pt20_trans.sort_values(by='valor',ascending=False)
+    pt20_trans['valor']=round(pt20_trans['valor'],2)
+    graf20=px.pie(pt20_trans,names='componente',values='valor', hole=.3, color_discrete_sequence=px.colors.sequential.Oranges_r)
+    graf20.update_layout(
+          title={'text':'Peaje de acceso 2.0','x':.5,'xanchor':'center'}
+    )
+
+    pt30=dffm.pivot_table(
+        values=['spot', 'ssaa', 'osom', 'Otros', 'ppcc_3.0', 'perd_3.0', 'pyc_3.0'],
+        index='año',
+        aggfunc='mean'
+    )
+    pt30['comp_perd']=pt30['spot']+pt30['ssaa']+pt30['osom']+pt30['Otros']+pt30['ppcc_3.0']
+    pt30['perdidas_3.0']=pt30['comp_perd']*(pt30['perd_3.0'])
+    pt30=pt30.drop(columns=['perd_3.0','comp_perd'])
+    pt30_trans=pt30.transpose().reset_index()
+    pt30_trans=pt30_trans.rename(columns={'index':'componente',2024:'valor'})
+    pt30_trans['componente'] = pt30_trans['componente'].replace({'Otros': 'otros'})
+    pt30_trans=pt30_trans.sort_values(by='valor',ascending=False)
+    pt30_trans['valor']=round(pt30_trans['valor'],2)
+    graf30=px.pie(pt30_trans,names='componente',values='valor', hole=.3, color_discrete_sequence=px.colors.sequential.Reds_r)
+    graf30.update_layout(
+          title={'text':'Peaje de acceso 3.0','x':.5,'xanchor':'center'}
+    )
+
+    pt61=dffm.pivot_table(
+        values=['spot', 'ssaa', 'osom', 'Otros', 'ppcc_6.1', 'perd_6.1', 'pyc_6.1'],
+        index='año',
+        aggfunc='mean'
+    )
+    pt61['comp_perd']=pt61['spot']+pt61['ssaa']+pt61['osom']+pt61['Otros']+pt61['ppcc_6.1']
+    pt61['perdidas_6.1']=pt61['comp_perd']*(pt61['perd_6.1'])
+    pt61=pt61.drop(columns=['perd_6.1','comp_perd'])
+    pt61_trans=pt61.transpose().reset_index()
+    pt61_trans=pt61_trans.rename(columns={'index':'componente',2024:'valor'})
+    pt61_trans['componente'] = pt61_trans['componente'].replace({'Otros': 'otros'})
+    pt61_trans=pt61_trans.sort_values(by='valor',ascending=False)
+    pt61_trans['valor']=round(pt61_trans['valor'],2)
+    graf61=px.pie(pt61_trans,names='componente',values='valor', hole=.3, color_discrete_sequence=px.colors.sequential.Blues_r)
+    graf61.update_layout(
+          title={'text':'Peaje de acceso 6.1','x':.5,'xanchor':'center'}
+    )
     
-    return pt1
+    return pt1, graf20, graf30, graf61
 
 # %%
 pt1()
@@ -125,7 +179,7 @@ pt1()
 
 # %%
 def pt1_trans():
-    pt2=pt1()
+    pt2=pt1()[0]
     pt1_trans=pt2.transpose()
     pt1_trans=pt1_trans.drop(['hora'])
     pt1_trans.columns.name='peajes'
@@ -138,11 +192,11 @@ def pt1_trans():
 
 # %%
 def graf_pt1():
-    pt2=pt1()
+    pt2=pt1()[0]
     colores_precios = {'precio_2.0': 'goldenrod', 'precio_3.0': 'darkred', 'precio_6.1': 'blue'}
     graf_pt1=px.line(pt2,x='hora',y=['precio_2.0','precio_3.0','precio_6.1'],
         height=600,
-        width=1000,
+        #width=1000,
         title="Telemindex 2024: Precios medios horarios de indexado según tarifas de acceso",
         labels={'value':'€/MWh','variable':'Precios s/ ATR'},
         color_discrete_map=colores_precios,
@@ -162,6 +216,11 @@ def graf_pt1():
     graf_pt1.update_layout(
         margin=dict(t=100),
         title_font_size=16,
+        title={'x':.5,'xanchor':'center'},
+        xaxis=dict(
+              tickmode='array',
+              tickvals=pt2['hora']
+        )
     )
     graf_pt1=graf_pt1.add_bar(y=pt2['spot'], name='spot', marker_color='green', width=0.5)
     
--- telemindex.py
+++ telemindex.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import globals
 
-from backend import  max_reg, lista_meses, filtrar_mes, aplicar_margen, pt1_trans,graf_pt1, pt5_trans
+from backend import  max_reg, lista_meses, aplicar_margen, pt1_trans, graf_pt1, pt5_trans, pt1
 
 
 
@@ -17,8 +17,10 @@ st.set_page_config(
         }
     )
 
-if 'mes_seleccionado' not in st.session_state: st.session_state.mes_seleccionado= None
-if 'margen' not in st.session_state: st.session_state.margen=0
+if 'mes_seleccionado' not in st.session_state: 
+    st.session_state.mes_seleccionado= None
+if 'margen' not in st.session_state: 
+    st.session_state.margen = 0
 
 #elementos de la barra lateral
 
@@ -56,13 +58,13 @@ aplicar_margen(globals.mes_seleccionado,globals.margen_aplicado)
 #st.sidebar.write("st.session_state object:", st.session_state)
 
 #ejecutamos la función para obtener la tabla resumen y precios medios
-pt6_trans, media_20,media_30,media_61,media_spot=pt5_trans()
+pt6_trans, media_20, media_30, media_61, media_spot=pt5_trans()
 media_20 =round(media_20 / 10,1)
 media_30 =round(media_30 / 10,1)
 media_61 =round(media_61 / 10,1)
 media_spot=round(media_spot,2)
 #ejecutamos la función para graficar
-graf_pt2=graf_pt1()
+#graf_pt2=graf_pt1()
 #ejecutamos la función para obtener la tabla de valores de la gráfica
 pt1_trans2=pt1_trans()
 
@@ -76,20 +78,11 @@ url_apps = "https://powerappspy-josevidal.streamlit.app/"
 st.write("Visita mi página de [PowerAPPs](%s) con un montón de utilidades" % url_apps)
 
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([.7,.3])
 
+#COLUMNA PRINCIPAL
 with col1:
-    st.subheader("Tabla resumen de precios minoristas por peaje de acceso", divider='rainbow')
-    with st.container():
-        col3, col4=st.columns([0.65,0.35])
-        with col3:
-            texto_precios=f'{texto_precios}. Precios en c€/kWh'
-            st.caption(texto_precios)
-            st.write(pt6_trans,)
-        with col4:
-            with st.expander("Nota sobre la Fórmula de indexado:"):
-                st.caption("Se incluye fnee, SRAD y 2€ en desvíos. Añadir margen al gusto en 'opciones' de la barra lateral")
-    st.subheader("Resumen de precios medios minoristas por peaje de acceso", divider='rainbow')
+    st.subheader("Resumen de precios medios minoristas por peaje de acceso. Totales y horarios.", divider='rainbow')
     st.caption(texto_precios)
     with st.container():
         col5, col6,col7,col8=st.columns(4)
@@ -101,12 +94,35 @@ with col1:
             st.metric(':blue[Precio medio 6.1]',value=media_61)
         with col8:
             st.metric(':green[Precio medio Spot €/MWh]',value=media_spot)
-        
+    st.empty()
+    st.plotly_chart(graf_pt1())
+    st.empty()
+    st.subheader("Peso de los componentes por peaje de acceso", divider='rainbow')
+    _, graf20, graf30, graf61=pt1()
+    col10,col11,col12=st.columns(3)
+    with col10:
+        st.write(graf20)    
+    with col11:
+        st.write(graf30)
+    with col12:
+        st.write(graf61)    
     
 
 with col2:
-    st.plotly_chart(graf_pt2)
+    
+    st.subheader("Tabla resumen de precios por peaje de acceso", divider='rainbow')
+    with st.container():
+        #col3, col4=st.columns([0.65,0.35])
+        #with col3:
+        texto_precios=f'{texto_precios}. Precios en c€/kWh'
+        st.caption(texto_precios)
+        st.write(pt6_trans,)
+        #with col4:
+        with st.expander("Nota sobre la Fórmula de indexado:"):
+            st.caption("Se incluye fnee, SRAD y 2€ en desvíos. Añadir margen al gusto en 'opciones' de la barra lateral")
+        if st.checkbox ('Mostrar tabla de datos de la gráfica'): 
+            st.write(pt1_trans2)
 
-    if st.checkbox ('Mostrar tabla de datos de la gráfica'): 
-        st.write(pt1_trans2)
+
+
 
